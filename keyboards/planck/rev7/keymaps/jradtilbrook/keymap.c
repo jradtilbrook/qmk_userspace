95f8d3f4080596ffa47729cc43eb13b607a2f5ed
jradtilbrook <jarryd@tilbrooktech.com>
2024-05-15 07:09:10 +0800

    Revert "Change raise/lower to OSL"
    This reverts commit 03b08e54768d1b27cd90aec3d83da60b2753caf2.

--- keyboards/planck/rev7/keymaps/jradtilbrook/keymap.c
+++ keyboards/planck/rev7/keymaps/jradtilbrook/keymap.c
@@ -20,9 +20,8 @@
 enum planck_layers { _QWERTY, _NAV, _LOWER, _RAISE, _ADJUST, _VIM };
 
 #define NAV MO(_NAV)
-#define LOWER OSL(_LOWER)
-#define RAISE OSL(_RAISE)
-#define VIM(kc) LT(_VIM, kc)
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
 #define HMSPN KC_F19
 #define RAYCAST KC_F20
 
@@ -43,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     HYPR_T(KC_ESC),LGUI_T(KC_A),LALT_T(KC_S),LSFT_T(KC_D),LCTL_T(KC_F),KC_G,KC_H,RCTL_T(KC_J),RSFT_T(KC_K),RALT_T(KC_L),RGUI_T(KC_SCLN), KC_QUOT,
-    NAV, VIM(KC_Z),KC_X,   KC_C,    KC_V,    KC_B,      KC_N,LSG_T(KC_M),    KC_COMM, KC_DOT,KC_SLSH, KC_ENT ,
+    NAV, LT(_VIM,KC_Z),KC_X,   KC_C,    KC_V,    KC_B,      KC_N,LSG_T(KC_M),    KC_COMM, KC_DOT,KC_SLSH, KC_ENT ,
     XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI, LOWER,   KC_LSFT, KC_SPC,  RAISE,    QK_REP, XXXXXXX, XXXXXXX,   XXXXXXX
 ),
 
