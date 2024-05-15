af52139502c39a8b6edc8662ca923376e5ba4266
jradtilbrook <jarryd@tilbrooktech.com>
2024-05-15 11:51:40 +0800

    Add pipe

--- keyboards/planck/rev7/keymaps/jradtilbrook/keymap.c
+++ keyboards/planck/rev7/keymaps/jradtilbrook/keymap.c
@@ -43,13 +43,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     HYPR_T(KC_ESC),LGUI_T(KC_A),LALT_T(KC_S),LSFT_T(KC_D),LCTL_T(KC_F),KC_G,KC_H,RCTL_T(KC_J),RSFT_T(KC_K),RALT_T(KC_L),RGUI_T(KC_SCLN), KC_QUOT,
     NAV, LT(_VIM,KC_Z),KC_X,   KC_C,    KC_V,    KC_B,      KC_N,LSG_T(KC_M),    KC_COMM, KC_DOT,KC_SLSH, KC_ENT ,
-    XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI, LOWER,   KC_LSFT, KC_SPC,  RAISE,    QK_REP, XXXXXXX, XXXXXXX,   XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI, LOWER,   KC_LSFT, KC_SPC,  RAISE,  XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX
 ),
 
 [_NAV] = LAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-    _______, _______, _______, _______, _______, _______,   HMSPN, RAYCAST, _______, _______, _______, _______,
+    _______, _______, _______, _______,LCTL_T(KC_LALT),_______,HMSPN, RAYCAST, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, KC_LALT, _______, _______, _______, _______, _______, _______
 ),
 
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      | Home | End  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_HOME, KC_END, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
