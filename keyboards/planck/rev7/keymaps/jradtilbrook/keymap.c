f85081266f560ca58c6a90de4b7e3aa21c900012
jradtilbrook <jarryd@tilbrooktech.com>
2024-11-28 11:14:27 +0800

    Update planck layout
    Mimicking corne v4 a bit to test if I should get one

--- keyboards/planck/rev7/keymaps/jradtilbrook/keymap.c
+++ keyboards/planck/rev7/keymaps/jradtilbrook/keymap.c
@@ -17,13 +17,10 @@
 #include QMK_KEYBOARD_H
 #include "quantum_keycodes.h"
 
-enum planck_layers { _QWERTY, _NAV, _LOWER, _RAISE, _ADJUST, _VIM };
+enum planck_layers { _QWERTY, _LOWER, _RAISE, _ADJUST };
 
-#define NAV MO(_NAV)
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
-#define HMSPN KC_F19
-#define RAYCAST KC_F20
 
 /* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -34,59 +31,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Nav  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |      |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      | GUI  |Lower | Shift|Space |Raise | Rpt  |      |      |      |
+ * |      |   (  |  [{  | GUI  |Lower | Shift|Space |Raise | CTRL |  }]  |  )   |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    HYPR_T(KC_ESC),LGUI_T(KC_A),LALT_T(KC_S),LSFT_T(KC_D),LCTL_T(KC_F),KC_G,KC_H,RCTL_T(KC_J),RSFT_T(KC_K),RALT_T(KC_L),RGUI_T(KC_SCLN), KC_QUOT,
-    NAV, LT(_VIM,KC_Z),KC_X,   KC_C,    KC_V,    KC_B,      KC_N,LSG_T(KC_M),    KC_COMM, KC_DOT,KC_SLSH, KC_ENT ,
-    XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI, LOWER,   KC_LSFT, KC_SPC,  RAISE,  XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX
-),
-
-[_NAV] = LAYOUT(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-    _______, _______, _______, _______,LCTL_T(KC_LALT),_______,HMSPN, RAYCAST, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, KC_LALT, _______, _______, _______, _______, _______, _______
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,    KC_BSPC,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,    KC_QUOT,
+    XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_SLSH,    KC_ENT ,
+    XXXXXXX, KC_LPRN, KC_LBRC, KC_LGUI, LOWER,   KC_LSFT, KC_SPC,  RAISE, KC_RCTL,KC_RBRC, KC_RPRN,   XXXXXXX
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |      |
+ * |      |      |      |EMOJI | REACT|      | HOME |      |      | END  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  |   |
+ * |      |      |STICKY|      |  =>  |  ->  |  <-  |  v   |  ^   |  ->  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      | Home | End  |      |
+ * |      |      |SCRTCH|SCSHOT|CLIPB | PASS |   -  |  =   |  _   |  +   |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_HOME, KC_END, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+        _______, _______, _______,G(C(KC_SPC)), _______,  _______,KC_HOME, _______, _______,   KC_END, _______, _______,
+        _______, _______,G(KC_EQL), _______, _______,  _______,   KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, _______, _______,
+        _______, _______,G(KC_MINS),KC_PSCR,LSG(KC_P),  _______,  KC_MINS,  KC_EQL, KC_UNDS,  KC_PLUS, _______,_______,
+        _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______,_______
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |      |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT(
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-    KC_GRV,LGUI_T(KC_1),LALT_T(KC_2),LSFT_T(KC_3),LCTL_T(KC_4),KC_5,KC_6,RCTL_T(KC_7),RSFT_T(KC_8),RALT_T(KC_9),RGUI_T(KC_0),KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+        KC_F1,   KC_F2, KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+       KC_GRV,    KC_1,  KC_2,   KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,     KC_9,    KC_0, KC_BSLS,
+      _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______,_______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
@@ -108,30 +98,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
-[_VIM] = LAYOUT(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,    KC_H,    KC_J,    KC_K,    KC_L, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-),
-
 };
 /* clang-format on */
 
 // shift + backspace = delete
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &delete_key_override,
-    NULL // Null terminate the array of overrides!
+const key_override_t *key_overrides[] = {
+    &delete_key_override
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     layer_state_t tri = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-    rgblight_set_layer_state(0, layer_state_cmp(tri, _NAV));
-    rgblight_set_layer_state(1, layer_state_cmp(tri, _LOWER));
-    rgblight_set_layer_state(2, layer_state_cmp(tri, _RAISE));
-    rgblight_set_layer_state(3, layer_state_cmp(tri, _ADJUST));
-    rgblight_set_layer_state(4, layer_state_cmp(tri, _VIM));
+    rgblight_set_layer_state(0, layer_state_cmp(tri, _LOWER));
+    rgblight_set_layer_state(1, layer_state_cmp(tri, _RAISE));
+    rgblight_set_layer_state(2, layer_state_cmp(tri, _ADJUST));
     return tri;
 }
 
@@ -144,9 +124,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 const rgblight_segment_t PROGMEM rgb_caps_word[] = RGBLIGHT_LAYER_SEGMENTS(
     {4, 2, HSV_RED}
 );
-const rgblight_segment_t PROGMEM rgb_nav_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {8, 1, HSV_MAGENTA}
-);
 const rgblight_segment_t PROGMEM rgb_lower_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {5, 2, HSV_PURPLE}
 );
@@ -156,18 +133,13 @@ const rgblight_segment_t PROGMEM rgb_raise_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 const rgblight_segment_t PROGMEM rgb_adjust_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {4, 2, HSV_WHITE}
 );
-const rgblight_segment_t PROGMEM rgb_vim_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {1, 1, HSV_MAGENTA}
-);
 const rgblight_segment_t PROGMEM rgb_bootloader[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 9, HSV_RED}
 );
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    rgb_nav_layer,
     rgb_lower_layer,
     rgb_raise_layer,
     rgb_adjust_layer,
-    rgb_vim_layer,
     rgb_caps_word,
     rgb_bootloader
 );
